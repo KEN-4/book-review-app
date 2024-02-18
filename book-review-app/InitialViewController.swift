f9f1049a21a9b11fd547f0de107253cb92415989
KEN-4 <ssstututusss617@gmail.com>
2024-02-18 17:01:06 +0900

    Initial Commit

---
+++ book-review-app/InitialViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  book-review-app
+//
+//  Created by 内藤広貴 on 2024/02/18.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

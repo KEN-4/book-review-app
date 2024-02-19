99b982d2a7ae755bd78e7d7209663c5ed01927cb
KEN-4 <ssstututusss617@gmail.com>
2024-02-19 09:59:26 +0900

    feat: Add zoom-out animation to app icon on Launch Screen

--- book-review-app/InitialViewController.swift
+++ book-review-app/InitialViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class InitialViewController: UIViewController {
 
+    @IBOutlet weak var logoImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //少し縮小するアニメーション
+        UIView.animate(withDuration: 0.3,
+                                   delay: 1.0,
+                                   options: UIView.AnimationOptions.curveEaseOut,
+                                   animations: { () in
+            self.logoImage.transform = CGAffineTransformMakeScale(0.9, 0.9)
+        }, completion: { (Bool) in
+            
+        })
+        //拡大させて、消えるアニメーション
+        UIView.animate(withDuration: 0.2,
+                                   delay: 1.3,
+                                   options: UIView.AnimationOptions.curveEaseOut,
+                                   animations: { () in
+                self.logoImage.transform = CGAffineTransformMakeScale(1.2, 1.2)
+                self.logoImage.alpha = 0
+            }, completion: { (Bool) in
+                self.logoImage.removeFromSuperview()
+        })
+    }
 }
 
